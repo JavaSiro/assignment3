fb6c12feff9ee44e53adf36693978a449bb0d7d7
JavaSiro <javokhirsirojiddinov@gmail.com>
2025-01-10 13:10:20 +0900

    first commit

---
+++ functions.py
@@ -0,0 +1,47 @@
+#b
+# def calculator(first,second):
+#     print(f'addition is {first+second}')
+#     print(f'subtraction is {first-second}')
+# first=int(input('type in the first number: '))
+# second=int(input('type in the second number: '))
+# calculator(first,second)
+
+#c
+# def calculator():
+#     num = 0
+#     for i in range(1, 11):
+#         num += i
+#     return num
+# print(calculator())
+
+#d
+# def work():
+#     list=[]
+#     for i in range(50,101):
+#         if i%2==0:
+#             list.append(i)
+#     return list
+# print(work())
+
+#e
+# def detector(num):
+#     if num%2==0:
+#         return f'{num}' ' is even'
+#     elif not num%2==0:
+#         return f'{num}' ' is odd'
+#     else:
+#         return f'{num}' ' is 0'
+# num=int(input('type in ur number '))
+# print(detector(num))
+
+#f
+# text=str(input('type: '))
+# def reverse_string(text):
+#     reversed_string = text[::-1]
+#     print(reversed_string)
+# reverse_string(text)
+
+#h
+# def detect():
+#     return max(list(range(1,2001)))
+# print(detect())
