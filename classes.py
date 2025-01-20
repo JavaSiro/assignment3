47e672a66c809241b780d0f985594b23ee7609f8
JavaSiro <javokhirsirojiddinov@gmail.com>
2025-01-20 19:27:44 +0900

    .

--- classes.py
+++ classes.py
@@ -11,20 +11,20 @@
 # parameters = Transport(180, 1000)
 # parameters.display()
 #d
-class Transport:
-    def __init__(self, max_vel, distance):
-        self.vel = max_vel
-        self.dis = distance
-    def writevelocity(self):
-        print("velocity is: ", self.vel * 1.5)
-    def writedistance(self):
-        print("distance is: ", self.dis - self.dis * 0.2)
+# class Transport:
+#     def __init__(self, max_vel, distance):
+#         self.vel = max_vel
+#         self.dis = distance
+#     def writevelocity(self):
+#         print("velocity is: ", self.vel * 1.5)
+#     def writedistance(self):
+#         print("distance is: ", self.dis - self.dis * 0.2)
 
-class YengilAvto(Transport):
-    def __init__(self, max_vel, distance):
-        self.vel = max_vel
-        self.dis = distance
+# class YengilAvto(Transport):
+#     def __init__(self, max_vel, distance):
+#         self.vel = max_vel
+#         self.dis = distance
 
-car = YengilAvto(120, 500)
-car.writevelocity()
-car.writedistance()  
+# car = YengilAvto(120, 500)
+# car.writevelocity()
+# car.writedistance()  
